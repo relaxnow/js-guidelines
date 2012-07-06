fdba3c310afdbd61869525ec77e339b37de64212
Boy Baukema <boy@ibuildings.nl>
2012-07-06 14:27:06 +0200

    Updated documentation

--- js/global-static/myproj.sub.js
+++ js/global-static/myproj.sub.js
@@ -2,10 +2,23 @@ if (typeof MYPROJ === 'undefined') {
     var MYPROJ = {};
 }
 
-MYPROJ.SUB = (function ($) {
+/**
+ * This sub namespace of the MYPROJ namespace contains DOM stuff.
+ *
+ * @class
+ * @type {MYPROJ.SUB}
+ */
+MYPROJ.DOMSTUFF = (function ($) {
     "use strict";
 
     return {
-
+        /**
+         * Do stuff with jQuery, rebind $ to jQuery for compatibility
+         */
+        doStuffWithJQuery: function () {
+            $('form').hide('slow', function () {
+                $('form').show();
+            });
+        }
     };
 }(jQuery));
